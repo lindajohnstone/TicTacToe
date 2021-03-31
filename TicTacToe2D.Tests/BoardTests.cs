b78018d6b91a4bdca82ede5235abdd2976582f6a
Linda Johnstone <linda.johnstone@myob.com>
2021-03-31 13:23:36 +1100

    Updated tests to make them cleaner by adding methods with Boards

--- TicTacToe2D.Tests/BoardTests.cs
+++ TicTacToe2D.Tests/BoardTests.cs
@@ -7,6 +7,44 @@ namespace TicTacToe2D.Tests
 {
     public class BoardTests
     {
+        public Board BoardIsWinningBoardTrue() {
+            var initData = new FieldContents[][] {
+                new []{FieldContents.x, FieldContents.y, FieldContents.empty},
+                new []{FieldContents.x, FieldContents.y, FieldContents.empty},
+                new []{FieldContents.x, FieldContents.y, FieldContents.empty}
+            };
+            return new Board(initData);
+        }
+        
+        public Board BoardIsWinningBoardFalse() {
+            var initData = new FieldContents[][] {
+                new []{FieldContents.x,     FieldContents.x,     FieldContents.empty},
+                new []{FieldContents.y,     FieldContents.empty, FieldContents.y},
+                new []{FieldContents.empty, FieldContents.x,     FieldContents.y}
+            };
+            return new Board(initData);
+        }
+
+        public Board BoardIsWinningBoardXWinningColumn()
+        {
+            var initData = new FieldContents[][] {
+                new []{FieldContents.x, FieldContents.y, FieldContents.empty},
+                new []{FieldContents.x, FieldContents.y, FieldContents.empty},
+                new []{FieldContents.x, FieldContents.empty, FieldContents.empty}
+            };
+            return new Board(initData);
+        }
+
+        public Board BoardIsWinningBoardYWinningColumn()
+        {
+            var initData = new FieldContents[][] {
+                new []{FieldContents.x,     FieldContents.y, FieldContents.empty},
+                new []{FieldContents.x,     FieldContents.y, FieldContents.x},
+                new []{FieldContents.empty, FieldContents.y, FieldContents.empty}
+            };
+            return new Board(initData);
+        }
+
         [Theory]
         [InlineData(3)]
         public void Board_dimensions(int dimensionLength)
@@ -18,20 +56,38 @@ namespace TicTacToe2D.Tests
         }
 
         [Theory]
-        [InlineData(0, 0, FieldContents.empty)]
-        [InlineData(0, 1, FieldContents.empty)]
+        [InlineData(0, 0, FieldContents.x)]
+        [InlineData(0, 1, FieldContents.y)]
         [InlineData(0, 2, FieldContents.empty)]
-        [InlineData(1, 0, FieldContents.empty)]
-        [InlineData(1, 1, FieldContents.empty)]
+        [InlineData(1, 0, FieldContents.x)]
+        [InlineData(1, 1, FieldContents.y)]
         [InlineData(1, 2, FieldContents.empty)]
-        [InlineData(2, 0, FieldContents.empty)]
-        [InlineData(2, 1, FieldContents.empty)]
+        [InlineData(2, 0, FieldContents.x)]
+        [InlineData(2, 1, FieldContents.y)]
         [InlineData(2, 2, FieldContents.empty)]
-        public void Board_contents_field_valid(int x, int y, FieldContents expected)
+        public void Board_has_valid_fields(int x, int y, FieldContents expected)
         {
             var position = new Position(x, y);
-            var result = new Board(3).GetField(position);
+            var result = BoardIsWinningBoardTrue().GetField(position);
             Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void IsWinningBoard_true()
+        {
+            var board = new Board(BoardIsWinningBoardTrue());
+            var win = new GamePredicate();
+            Assert.Equal(true, win.IsWinningBoard(board, board.GetWinningLines(), FieldContents.x));
+            Assert.Equal(true, win.IsWinningBoard(board, board.GetWinningLines(), FieldContents.y));
+        }
+
+        [Fact]
+        public void IsWinningBoard_false()
+        {
+            var board = new Board(BoardIsWinningBoardFalse());
+            var win = new GamePredicate();
+            Assert.Equal(false, win.IsWinningBoard(board, board.GetWinningLines(), FieldContents.x));
+            Assert.Equal(false, win.IsWinningBoard(board, board.GetWinningLines(), FieldContents.y));
         }
 
         [Fact] 
@@ -50,6 +106,22 @@ namespace TicTacToe2D.Tests
             var board = new Board(3);
             var result = Assert.Throws<ArgumentException>(() => board.GetField(position));
             Assert.Equal("Position X coordinate is out of range", result.Message);
+        }
+
+        [Fact]
+        public void IsWinningBoard_X_wins()
+        {
+            var board = new Board(BoardIsWinningBoardXWinningColumn());
+            var win = new GamePredicate();
+            Assert.Equal(true, win.IsWinningBoard(board, board.GetWinningLines(), FieldContents.x));
+        }
+
+        [Fact]
+        public void IsWinningBoard_Y_wins()
+        {
+            var board = new Board(BoardIsWinningBoardYWinningColumn());
+            var win = new GamePredicate();
+            Assert.Equal(true, win.IsWinningBoard(board, board.GetWinningLines(), FieldContents.y));
         }
 
         [Fact]
